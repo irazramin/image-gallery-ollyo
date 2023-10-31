046bd5cb28b66d1ed9b2cdce998cd55a43babf7a
irazramin <irazramin@gmail.com>
2023-10-31 15:16:23 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from "./pages/home/home-page";
+
+function App() {
+  return (
+    <div className="bg-blue-50 min-h-screen p-[50px]">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/home-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavbarComponent from "../../components/layouts/Navbar.component";
+
+const HomePage = () => {
+    return (
+        <div className="container mx-auto">
+            <NavbarComponent />
+            Home
+        </div>
+    );
+};
+
+export default HomePage;
